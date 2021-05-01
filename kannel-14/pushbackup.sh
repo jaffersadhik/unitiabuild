561e61b40d6be06129b97315f1ed4862b486d496
root <root@ip-10-0-2-14.ap-south-1.compute.internal>
2021-05-01 19:39:47 +0530

    jj

--- kannel-14/pushbackup.sh
+++ kannel-14/pushbackup.sh
@@ -1,3 +1,5 @@
 cp -rp dnsql /home/jaffer/unitiabuild/kannel-14/.
 cp -rp kannel /home/jaffer/unitiabuild/kannel-14/.
+cp -rp vedioconkannel /home/jaffer/unitiabuild/kannel-14/.
+cp -rp vedioconkanneldnsql /home/jaffer/unitiabuild/kannel-14/.
 cp -rp pushbackup.sh /home/jaffer/unitiabuild/kannel-14/.
