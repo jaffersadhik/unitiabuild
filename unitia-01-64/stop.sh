671e7989351130f62c4d8967f6f55e5422bdaa79
jaffersadhik <jaffer.sadhik@gmail.com>
2021-05-30 07:14:06 +0530

    jj

--- unitia-01-64/stop.sh
+++ unitia-01-64/stop.sh
@@ -2,71 +2,47 @@ cd /home/build
 ./httprestart.sh
 cd /home/build/router
 ./down.sh
-./start.sh
 cd /home/build/kannelconnector2
 ./down.sh
-./start.sh
 cd /home/build/cdacconnector
 ./down.sh
-./start.sh
 cd /home/build/requestlog
 ./down.sh
-./start.sh
 cd /home/build/dnquerylog
 ./down.sh
-./start.sh
 cd /home/build/submission
 ./down.sh
-./start.sh
 cd /home/build/dn
 ./down.sh
-./start.sh
 cd /home/build/delivery
 ./down.sh
-./start.sh
 cd /home/build/dnhttppost
 ./down.sh
-./start.sh
 cd /home/build/dnpostdb
 ./down.sh
-./start.sh
 cd /home/build/scheduledb
 ./down.sh
-./start.sh
 cd /home/build/tablereader
 ./down.sh
-./start.sh
 cd /home/build/queuecheck
 ./down.sh
-./start.sh
 cd /home/build/creditupdate
 ./down.sh
-./start.sh
 cd /home/build/smpp1
 ./down.sh
-./start.sh
 cd /home/build/smpp2
 ./down.sh
-./start.sh
 cd /home/build/dnsqlA
 ./down.sh
-./start.sh
 cd /home/build/dnsqlB
 ./down.sh
-./start.sh
 cd /home/build/kannelA
 ./down.sh
-./start.sh
 cd /home/build/kannelB
 ./down.sh
-./start.sh
 cd /home/build/aux
 ./down.sh
-./start.sh
 cd /home/build/dbtoredis
 ./down.sh
-./start.sh
 cd /home/build/reroutekannel
 ./down.sh
-./start.sh
-
